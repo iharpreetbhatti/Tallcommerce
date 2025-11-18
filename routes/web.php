e70dc412dc2163e572c5d6063b21c5d76d4394f3
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-11-18 16:46:45 +0530

    added route to handle get requests on admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Pages\Admin\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/admin/dashboard', Dashboard::class)
+    ->name('dashboard');
