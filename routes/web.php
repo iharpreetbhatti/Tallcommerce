3e78a1a50017f327eaa423e10573cf8aca9da5c5
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-12-22 11:22:13 +0530

    added get and post routes for login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Admin\Dashboard;
+use App\Livewire\Pages\Login;
+use App\Livewire\LoginForm;
 
-Route::get('/admin/dashboard', Dashboard::class)
+Route::get('/admin/dashboard', Dashboard::class)->middleware('auth')
     ->name('dashboard');
+
+Route::get('/login', Login::class)->middleware('guest')->name('login');
+
+Route::post('/login', LoginForm::class)->middleware('guest')->name('login.authenticate');
 
 Route::redirect('/admin', '/admin/dashboard');
