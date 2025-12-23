9f2dd21595ce6fa4ea8254e6da07b76e244fbd3e
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-12-23 10:44:51 +0530

    removed post route for login form

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Admin\Dashboard;
 use App\Livewire\Pages\Login;
-use App\Livewire\LoginForm;
 
 Route::get('/admin/dashboard', Dashboard::class)->middleware('auth')
     ->name('dashboard');
 
 Route::get('/login', Login::class)->middleware('guest')->name('login');
 
-Route::post('/login', LoginForm::class)->middleware('guest')->name('login.authenticate');
+// Route::post('/login', [LoginForm::class, 'login'])->middleware('guest')->name('login.authenticate');
 
 Route::redirect('/admin', '/admin/dashboard');
