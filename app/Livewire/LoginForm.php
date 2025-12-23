487e38a20ee27d6c4936813fd715070b29867e36
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-12-23 10:45:26 +0530

    added validation and login logic

--- app/Livewire/LoginForm.php
+++ app/Livewire/LoginForm.php
@@ -3,20 +3,35 @@
 namespace App\Livewire;
 
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class LoginForm extends Component
 {
-    protected $email = '';
-    protected $password = '';
+    #[Validate('required|email')]
+    public $email = '';
+
+    #[Validate('required|min:6')]
+    public $password = '';
+
+    public function login()
+    {
+        $credentials = [
+            'email' => $this->email,
+            'password' => $this->password,
+        ];
+
+        if (Auth::attempt($credentials)) {
+            session()->regenerate();
+
+            return $this->redirectIntended('/admin/dashboard', navigate: true);
+        }
+
+        $this->addError('email', 'The provided credentials do not match our records.');
+    }
 
     public function render()
     {
         return view('livewire.login-form');
-    }
-
-    public function login()
-    {
-
     }
 }
