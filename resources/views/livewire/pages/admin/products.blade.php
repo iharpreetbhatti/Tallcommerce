92185e2b93b644cd192d9f67f00f14a7c0066df8
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2026-01-28 17:22:15 +0530

    views: create Products admin page view

---
+++ resources/views/livewire/pages/admin/products.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-gray-50 py-4 min-h-screen">
+  <div class="container mx-auto">
+    <h1 class="text-xl font-medium my-2">Products</h1>
+    <p class="text-gray-500 text-lg mb-4">Manage your product inventory</p>
+    <livewire:components.product-list />
+  </div>
+</div>
