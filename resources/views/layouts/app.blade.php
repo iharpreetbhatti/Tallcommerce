da691600fefd983e0818f19d4596b31c8851c255
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2026-01-28 17:13:25 +0530

    added vite livewire hot reloading plugin to app view & script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>{{ $title ?? 'Page Title' }} - Tallcommerce</title>
-
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <title>{{ $title ?? 'Page Title' }} - Tallcommerce</title>
 </head>
 
 <body>
   {{ $slot }}
+
+  @livewireScripts()
 </body>
 
 </html>
