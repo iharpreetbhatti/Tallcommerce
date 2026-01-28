917a42ab82a879aeabe3466b0f774954b518ba80
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2026-01-28 17:22:26 +0530

    routes: add products admin page route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Admin\Dashboard;
+use App\Livewire\Pages\Admin\Products;
 use App\Livewire\Pages\Login;
 
 Route::get('/admin/dashboard', Dashboard::class)->middleware('auth')
     ->name('dashboard');
+
+Route::get('/admin/products', Products::class)->middleware('auth')
+    ->name('products');
 
 Route::get('/login', Login::class)->middleware('guest')->name('login');
 
