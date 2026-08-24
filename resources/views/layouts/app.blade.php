7161358fcacc10c0b2dd2b20b8a164255e478788
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2026-08-24 16:45:10 +0530

    added livewire styles and scripts directive to the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @livewireStyles
   <title>{{ $title ?? 'Page Title' }} - Tallcommerce</title>
 </head>
 
 <body>
-  {{ $slot }}
+{{ $slot }}
 
-  @livewireScripts()
+@livewireScripts
 </body>
 
 </html>
