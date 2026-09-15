9a04307899c77d2a3c8ed54f3a55b8023f18ece0
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2026-09-15 12:54:59 +0530

    fix: added return type on render method

--- app/Livewire/Pages/Admin/Dashboard.php
+++ app/Livewire/Pages/Admin/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Pages\Admin;
 
+use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
@@ -10,7 +11,7 @@ use Livewire\Attributes\Title;
 class Dashboard extends Component
 {
     #[Title('Dashboard')]
-    public function render()
+    public function render(): View
     {
         return view('livewire.pages.admin.dashboard');
     }
